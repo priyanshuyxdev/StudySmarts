2931587ec51c4ffc3ab68b50df97fca900e4950f
priyanshuyxdev <priyanshuyxdev@gmail.com>
2025-05-25 01:00:57 +0530

    fixed2

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export', // Added for static site generation
+  // output: 'export', // Added for static site generation
   typescript: {
     ignoreBuildErrors: true,
   },
